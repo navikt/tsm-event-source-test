56ee41491abbbf4bd852313ffffda2e11b18066b
Karl J. Overå <k@rl.run>
2024-03-14 13:50:58 +0100

    chore: use later offsets in prod

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { startConsumer, stopConsumer } from './kafka/consumer-start.ts'
+
 import logger from './utils/logger.ts'
 import { configureApi } from './api.ts'
 import { configureCron } from './cron.ts'
+// import { startConsumer, stopConsumer } from './kafka/one-off/consumer-find-evergreen-offset.ts'
 
 process.on('exit', async (code) => {
     if (code === 0) {
